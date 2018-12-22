5a1ff014f54ec65d8fc00cf9cc5e105cdfc2c83c
xiaoming <fymhunan@163.com>
2018-12-22 12:19:26 +0800

    解决maven compile阶段mybatis generator会自动生成代码的BUG

--- src/main/java/com/nexwise/controller/UserController.java
+++ src/main/java/com/nexwise/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.nexwise.controller;
 
-import com.nexwise.dao.UsersMapper;
-import com.nexwise.entity.Users;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
